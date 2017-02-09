08d9af45c883b469f1de42227c1f35386e5655d0
Steffen Bauer <steffen.aus.karlsruhe@googlemail.com>
2017-02-09 15:35:57 +0100

    Add register & invitation sequence diagrams

---
+++ python/mia_client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+HOST = 'localhost'
+PORT = 4080
+
+def mia_client_start():
+  pass
+
+if __name__ == "__main__":
+    mia_client_start()
---
+++ python/mia_server.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+HOST = ''
+PORT = 4080
+
+def mia_server_start():
+  pass
+
+if __name__ == "__main__":
+    mia_server_start()
