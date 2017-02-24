9a23ef917dbd22ad30237ca51ed4d5d43b1bfa4d
Steffen Bauer <steffen.aus.karlsruhe@googlemail.com>
2017-02-24 16:04:24 +0100

    Add python mia server UDP loop

--- python/mia_server.py
+++ python/mia_server.py
@@ -3,8 +3,33 @@
 HOST = ''
 PORT = 4080
 
+DEBUG = True
+
+def handle_packet(sock, addr, data):
+  if data.startswith("REGISTER;"):
+    response = register_player(data, addr)
+  elif data.startswith("REGISTER_SPECTATOR"):
+    response = register_spectator(data, addr)
+
+  if response:
+    if DEBUG: print "Replying '",response,"'"
+    sock.sendto(response, addr)
+  else:
+    if DEBUG: print "No response"
+
 def mia_server_start():
-  pass
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  sock.bind((HOST, PORT))
+  while 1:
+    try:
+      data, addr = sock.recvfrom(1024)
+      if DEBUG: print "Received", data.strip(), "from", addr
+      handle_packet(sock, addr, data.strip())
+    except KeyboardInterrupt:
+      print "Shutting down server"
+      break
+    except: pass
+  s.close()
 
 if __name__ == "__main__":
     mia_server_start()
