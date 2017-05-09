82d6cc8b2ea98514cdf0991d8661ab35ce59241d
Steffen Bauer <steffen.aus.karlsruhe@googlemail.com>
2017-05-09 14:52:51 +0200

    Add additional player and spectator to gameplay tests

--- macos_localhost.sh
+++ macos_localhost.sh
@@ -4,3 +4,4 @@ sudo ifconfig lo0 alias 127.0.0.2 up
 sudo ifconfig lo0 alias 127.0.0.3 up
 sudo ifconfig lo0 alias 127.0.0.4 up
 sudo ifconfig lo0 alias 127.0.0.5 up
+sudo ifconfig lo0 alias 127.0.0.6 up
--- mia_server/test/mia_server/gameplay_test.exs
+++ mia_server/test/mia_server/gameplay_test.exs
@@ -2,6 +2,10 @@ defmodule MiaServer.GameplayTest do
   use ExUnit.Case, async: false
   require Logger
 
+  @timeout round(Application.get_env(:mia_server, :timeout) * 1.25)
+  @players 2
+  @spectators 2
+
   setup do
     Application.start(:mia_server)
     on_exit fn ->
@@ -9,10 +13,6 @@ defmodule MiaServer.GameplayTest do
       Logger.flush()
     end
   end
-
-  @timeout round(Application.get_env(:mia_server, :timeout) * 1.25)
-  @players 2
-  @spectators 1
 
   defp open_udp_socket(ip) do
     opts = [:binary, active: false, ip: ip]
@@ -70,21 +70,30 @@ defmodule MiaServer.GameplayTest do
     {socket1, port1} = open_udp_socket({127,0,0,2})
     {socket2, port2} = open_udp_socket({127,0,0,3})
     {socket3, port3} = open_udp_socket({127,0,0,4})
+    {socket4, port4} = open_udp_socket({127,0,0,5})
+    {socket5, port5} = open_udp_socket({127,0,0,6})
     send_and_recv("REGISTER;player1", socket1, port1)
     send_and_recv("REGISTER;player2", socket2, port2)
-    send_and_recv("REGISTER_SPECTATOR", socket3, port3)
+    send_and_recv("REGISTER;player3", socket3, port3)
+    send_and_recv("REGISTER_SPECTATOR", socket4, port4)
+    send_and_recv("REGISTER_SPECTATOR", socket5, port5)
     token1 = receive_message(socket1) |> check_and_gettoken(:roundstarting)
     token2 = receive_message(socket2) |> check_and_gettoken(:roundstarting)
-    "ROUND STARTING\n" = receive_message(socket3)
+    _token3 = receive_message(socket3) |> check_and_gettoken(:roundstarting)
+    "ROUND STARTING\n" = receive_message(socket4)
+    "ROUND STARTING\n" = receive_message(socket5)
     send_to_server("JOIN;#{token1}", socket1, port1)
     send_to_server("JOIN;#{token2}", socket2, port2)
     receive_message(socket1)
     receive_message(socket2)
-    start = receive_message(socket3)
+    receive_message(socket3)
+    receive_message(socket4)
+    start = receive_message(socket5)
     %{:turn => nil,
       :players => nil,
-      :sockets => [socket1, socket2, socket3],
-      :ports => [port1, port2, port3]}
+      :registered => ["player1", "player2", "player3"],
+      :sockets => [socket1, socket2, socket3, socket4, socket5],
+      :ports => [port1, port2, port3, port4, port5]}
     |> extract_player_seq(start)
   end
 
@@ -100,15 +109,19 @@ defmodule MiaServer.GameplayTest do
       |> Enum.to_list
       |> Enum.map(fn n -> "#{game[:players][n][:name]}:#{if(n == playerno, do: 0, else: 1)}" end)
       |> Enum.sort()
-      |> Enum.join(","))
+      |> Enum.join(",")) <> ",player3:0"
+
     check_broadcast_message(game[:sockets], scoremsg <> "\n")
-    invites = for s <- game[:sockets] do
+
+    for n <- 1..5 do
+      s = Enum.at(game[:sockets], n-1)
       {:ok, {_ip, _port, invitation}} = :gen_udp.recv(s, 0, @timeout)
-      invitation
+      if n < 4 do
+        assert invitation =~ ~r/ROUND STARTING;[0-9a-fA-F]{32}\n/
+      else
+        assert invitation == "ROUND STARTING\n"
+      end
     end
-    [i | invites ] = invites; assert i =~ ~r/ROUND STARTING;[0-9a-fA-F]{32}\n/
-    [i | invites ] = invites; assert i =~ ~r/ROUND STARTING;[0-9a-fA-F]{32}\n/
-    assert invites == ["ROUND STARTING\n"]
   end
 
   defp make_roll_msg(token), do: "ROLL;#{token}"
